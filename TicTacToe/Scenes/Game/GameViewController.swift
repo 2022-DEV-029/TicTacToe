c8903467da6c74ec70cadfd6eb63edf444f66391
2022-DEV-029 <2022dev029@gmail.com>
2022-07-03 20:48:35 +0200

    added necessary UI elements and passed the test

--- TicTacToe/Scenes/Game/GameViewController.swift
+++ TicTacToe/Scenes/Game/GameViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+final class GameViewController: UIViewController {
 
+    @IBOutlet private var tileButtons: [UIButton]!
+    @IBOutlet private weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- TicTacToeTests/Scenes/Game/GameViewControllerTests.swift
+++ TicTacToeTests/Scenes/Game/GameViewControllerTests.swift
@@ -8,7 +8,7 @@
 @testable import TicTacToe
 import XCTest
 
-class GameViewControllerTests: XCTestCase {
+final class GameViewControllerTests: XCTestCase {
 
     // MARK: - Subject under test
     
@@ -40,5 +40,69 @@ class GameViewControllerTests: XCTestCase {
     func test_givenScene_whenViewIsLoaded_thenloadViewIsCalled() {
         XCTAssertTrue(sut.isViewLoaded)
     }
+    
+    // MARK: UI Elements
+    
+    func test_givenScene_whenViewIsLoaded_thenShouldShowUIElements() {
+        XCTAssertEqual(tileButtonsVisible(), true)
+        XCTAssertEqual(resetButtonVisible(), true)
+        XCTAssertEqual(infoLabelVisible(), true)
+    }
 
+}
+
+private extension GameViewControllerTests {
+    // MARK: UI Elements Check
+    
+    func tileButtonsVisible() -> Bool {
+        let mirror = Mirror.init(reflecting: sut.view as Any)
+        for child in mirror.children {
+            if let view = child.value as? UIView {
+                for subview in view.subviews {
+                    for subsubview in subview.subviews {
+                        if let stackView = subsubview as? UIStackView {
+                            for subStackView in stackView.subviews {
+                                if let horizontalStackView = subStackView as? UIStackView {
+                                    for button in horizontalStackView.subviews {
+                                        if let _ = button as? UIButton {
+                                            return true
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false
+    }
+    
+    func resetButtonVisible() -> Bool {
+        let mirror = Mirror.init(reflecting: sut.view as Any)
+        for child in mirror.children {
+            if let view = child.value as? UIView {
+                for subview in view.subviews {
+                    if let button = subview as? UIButton, button.titleLabel?.text == "Reset" {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+    
+    func infoLabelVisible() -> Bool {
+        let mirror = Mirror.init(reflecting: sut.view as Any)
+        for child in mirror.children {
+            if let view = child.value as? UIView {
+                for subview in view.subviews {
+                    if let _ = subview as? UILabel {
+                        return true
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
