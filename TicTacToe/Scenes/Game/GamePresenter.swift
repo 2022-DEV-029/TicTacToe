c8cfc4b4bb221e3e4cbf5f284ce8d90ddb4d3032
2022-DEV-029 <2022dev029@gmail.com>
2022-07-03 23:01:38 +0200

    added the output methods for the presenter

--- TicTacToe/Scenes/Game/GamePresenter.swift
+++ TicTacToe/Scenes/Game/GamePresenter.swift
@@ -7,7 +7,24 @@
 
 import Foundation
 
+typealias GamePresenterInput = GameInteractorOutput
+
+protocol GamePresenterOutput: AnyObject {
+    func displayGameMove(gameInfo: GameInfo)
+    func reset(gameInfo: GameInfo)
+}
 
 final class GamePresenter {
-  
+    weak var output: GamePresenterOutput?
 }
+
+extension GamePresenter: GamePresenterInput {
+    func presentGameMove(gameInfo: GameInfo) {
+        output?.displayGameMove(gameInfo: gameInfo)
+    }
+    
+    func presentReset(gameInfo: GameInfo) {
+        output?.reset(gameInfo: gameInfo)
+    }
+}
+
--- TicTacToeTests/Scenes/Game/GamePresenterTests.swift
+++ TicTacToeTests/Scenes/Game/GamePresenterTests.swift
@@ -12,19 +12,62 @@ class GamePresenterTests: XCTestCase {
     // MARK: Subject under test
     
     private var sut: GamePresenter!
+    private var output: GamePresenterOutputSpy!
     
     // MARK: Test lifecycle
     
     override func setUp() {
         super.setUp()
         
+        output = GamePresenterOutputSpy()
         sut = GamePresenter()
+        sut.output = output
     }
     
     override func tearDown() {
         sut = nil
+        output = nil
         
         super.tearDown()
     }
+    
+    func test_givenPresenter_whenDiplayGameMoveCalled_thenVCsProperMethodInvoked() {
+        sut.presentGameMove(gameInfo: GameInfo(tileIdentifer: nil, infoLabelText: nil, infoLabelBackground: nil, tileText: nil))
+        
+        XCTAssertTrue(output.diplayGameMoveCalled.0)
+    }
+    
+    func test_givenPresenter_whenResetCalled_thenVCsProperMethodInvoked() {
+        sut.presentReset(gameInfo: GameInfo(tileIdentifer: nil, infoLabelText: nil, infoLabelBackground: nil, tileText: nil))
+    
+        XCTAssertTrue(output.resetCalled.0)
+    }
+    
+    func test_givenPresenter_whenDiplayGameMoveCalled_thenVCsProperArgumentsPassed() {
+        let gameInfo = GameInfo(tileIdentifer: 1, infoLabelText: "Player X Move", infoLabelBackground: .clear, tileText: "O")
+        sut.presentGameMove(gameInfo: gameInfo)
+        
+        XCTAssertEqual(output.diplayGameMoveCalled.1, gameInfo)
+    }
+    
+    func test_givenPresenter_whenResetCalled_thenVCsProperArgumentsPassed() {
+        let gameInfo = GameInfo(tileIdentifer: nil, infoLabelText: "Player X Move", infoLabelBackground: .clear, tileText: nil)
+        sut.presentReset(gameInfo: gameInfo)
+    
+        XCTAssertEqual(output.resetCalled.1, gameInfo)
+    }
 
 }
+
+private final class GamePresenterOutputSpy: GamePresenterOutput {
+    var diplayGameMoveCalled: (Bool, GameInfo)!
+    var resetCalled: (Bool, GameInfo)!
+    
+    func displayGameMove(gameInfo: GameInfo) {
+        diplayGameMoveCalled = (true, gameInfo)
+    }
+    func reset(gameInfo: GameInfo) {
+        resetCalled = (true, gameInfo)
+    }
+}
+
